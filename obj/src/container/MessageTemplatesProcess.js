852220f0ac8bd48d20ffed026f3afba102bd53d2
Dmitriy Krayniy <krdima92@gmail.com>
2018-08-20 14:07:39 +0300

    added DefaultRpcFactory

--- obj/src/container/MessageTemplatesProcess.js
+++ obj/src/container/MessageTemplatesProcess.js
@@ -2,10 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const pip_services_container_node_1 = require("pip-services-container-node");
 const MessageTemplatesServiceFactory_1 = require("../build/MessageTemplatesServiceFactory");
+const pip_services_rpc_node_1 = require("pip-services-rpc-node");
 class MessageTemplatesProcess extends pip_services_container_node_1.ProcessContainer {
     constructor() {
         super("message_templates", "Message templates microservice");
         this._factories.add(new MessageTemplatesServiceFactory_1.MessageTemplatesServiceFactory);
+        this._factories.add(new pip_services_rpc_node_1.DefaultRpcFactory);
     }
 }
 exports.MessageTemplatesProcess = MessageTemplatesProcess;
--- src/container/MessageTemplatesProcess.ts
+++ src/container/MessageTemplatesProcess.ts
@@ -2,12 +2,14 @@ import { IReferences } from 'pip-services-commons-node';
 import { ProcessContainer } from 'pip-services-container-node';
 
 import { MessageTemplatesServiceFactory } from '../build/MessageTemplatesServiceFactory';
+import { DefaultRpcFactory } from 'pip-services-rpc-node';
 
 export class MessageTemplatesProcess extends ProcessContainer {
 
     public constructor() {
         super("message_templates", "Message templates microservice");
         this._factories.add(new MessageTemplatesServiceFactory);
+        this._factories.add(new DefaultRpcFactory);
     }
 
 }
