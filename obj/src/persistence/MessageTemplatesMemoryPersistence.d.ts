d549c55f5fa7646958376e467e59ea791859cb4b
pipdeveloper <pipdevs@gmail.com>
2019-07-09 14:13:46 +0000

    project build by Travis CI [skip ci]

--- obj/src/persistence/MessageTemplatesMemoryPersistence.d.ts
+++ obj/src/persistence/MessageTemplatesMemoryPersistence.d.ts
@@ -6,11 +6,11 @@ import { MessageTemplateV1 } from '../data/version1/MessageTemplateV1';
 import { IMessageTemplatesPersistence } from './IMessageTemplatesPersistence';
 export declare class MessageTemplatesMemoryPersistence extends IdentifiableMemoryPersistence<MessageTemplateV1, string> implements IMessageTemplatesPersistence {
     constructor();
-    private matchString;
-    private matchMultilanguageString;
-    private matchSearch;
-    private contains;
-    private composeFilter;
+    private matchString(value, search);
+    private matchMultilanguageString(value, search);
+    private matchSearch(item, search);
+    private contains(array1, array2);
+    private composeFilter(filter);
     getPageByFilter(correlationId: string, filter: FilterParams, paging: PagingParams, callback: (err: any, page: DataPage<MessageTemplateV1>) => void): void;
     getOneByIdOrName(correlationId: string, idOrName: string, callback: (err: any, item: MessageTemplateV1) => void): void;
 }
